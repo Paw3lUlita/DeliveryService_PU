e29674b4cf672ac7c435cfaacd28cfc11c57f2c0
Paw3lUlita <pawel.ulita33@gmail.com>
2023-01-30 12:37:02 +0100

    xml files and parsing with DOM

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -8,18 +8,36 @@ import com.solvd.services.AddressService;
 import com.solvd.services.DeliveryService;
 import com.solvd.services.OrderService;
 import com.solvd.services.UserService;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserService();
-        OrderService orderService = new OrderService();
-        DeliveryService deliveryService = new DeliveryService();
-        AddressService addressService = new AddressService();
+        Document doc = null;
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            doc = builder.parse("src/main/resources/xml_files/user.xml");
+            Node userNode = doc.getElementsByTagName("user").item(0);
+            System.out.println("user id: " + userNode.getAttributes().getNamedItem("id").getTextContent());
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
 
-        User user = userService.getUserById(1);
-        Address address = addressService.getAddressById(9);
-        orderService.addOrderForUser(user, address);
-
-        orderService.getAllOrders().forEach(System.out::println);
+        System.out.println("name: " + doc.getElementsByTagName("name").item(0).getTextContent());
+        System.out.println("surname: " + doc.getElementsByTagName("surname").item(0).getTextContent());
+        System.out.println("phoneNumber: " + doc.getElementsByTagName("phoneNumber").item(0).getTextContent());
+        System.out.println("email: " + doc.getElementsByTagName("email").item(0).getTextContent());
+        System.out.println("Address: ");
+        System.out.println("address id: " + doc.getElementsByTagName("address").item(0)
+                .getAttributes().getNamedItem("id").getTextContent());
+        System.out.println(doc.getElementsByTagName("street").item(0).getTextContent());
+        System.out.println(doc.getElementsByTagName("house_number").item(0).getTextContent());
+        System.out.println(doc.getElementsByTagName("postcode").item(0).getTextContent());
     }
 }
